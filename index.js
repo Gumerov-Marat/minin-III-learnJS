d84fec8948bd6ebe87958520d042010ec620b88e
Marat Gumerov <gumerovm74@gmail.com>
2020-06-24 22:40:56 +0300

    async Await

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 document.querySelector('#load').addEventListener('click', load)
 
-function load() {
+async function load() {
   var url = 'https://jsonplaceholder.typicode.com/users'
 
-  fetch(url)
-    .then(function (response) {
-      return response.json()
-    })
-    .then(function (data) {
-      var ul = document.querySelector('#list')
+  // fetch(url)
+  //   .then(function (response) {
+  //     return response.json()
+  //   })
+  //   .then(function (data) {
+  //     var ul = document.querySelector('#list')
 
-      var html = data.map(function (item) {
-        return '<li>' + item.id + ' ' + item.name + ' (' + item.email + ')</li>'
-      })
+  //     var html = data.map(function (item) {
+  //       return '<li>' + item.id + ' ' + item.name + ' (' + item.email + ')</li>'
+  //     })
 
-      ul.insertAdjacentHTML('afterbegin', html.join(' '))
-    })
+  //     ul.insertAdjacentHTML('afterbegin', html.join(' '))
+  //   })
+
+  var response = await fetch(url)
+  var data = await response.json()
+
+  var html = data.map(function (item) {
+    return '<li>' + item.id + ' ' + item.name + ' (' + item.email + ')</li>'
+  })
+  document.querySelector('#list').insertAdjacentHTML('afterbegin', html.join(' '))
 }
