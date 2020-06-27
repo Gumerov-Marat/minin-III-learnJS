ec84a50cbb5398a610b8a902cf00c2df18438182
Marat Gumerov <gumerovm74@gmail.com>
2020-06-27 23:51:16 +0300

    восстановлен код

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // импортируем обязательно
-const merge = require('webpack-merge');
-const pug = require('./webpack/pug');
-const devserver = require('./webpack/devserver');
-const sass = require('./webpack/sass');
-const images = require('./webpack/images');
-const fonts = require('./webpack/fonts');
 
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
-const PATHS = {
-  source: path.join(__dirname, 'src'),
-  build: path.join(__dirname, 'build')
-};
-
-const common = merge([{
-    mode: 'development',
-    resolve: {
-      alias: {
-        Src: path.resolve(__dirname, './src'),
-      }
-    },
-    entry: {
-      'index': PATHS.source + '/index.js',
-    },
-
-    output: {
-      path: PATHS.build,
-      filename: 'js/[name].js'
-    },
-
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: PATHS.source + '/index.pug'
-      }),
-
-      // Это важный блок и он тут нужен. Ввверху импорт плагина
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-      }),
-    ],
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
   },
-  pug(),
-  sass(),
-  images(),
-  fonts()
-]);
-
-
-module.exports = function (env) {
-  if (env === 'production') {
-    return common;
+  devServer: {
+    contentBase: __dirname + '/dist'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ]
+        }
+      }
+    }]
   }
-  if (env === 'development') {
-    return merge([
-      common,
-      devserver()
-    ]);
-  }
-};
+}
