c3679fc64989e37f2e82544aec56bd03c41f7eaa
Marat Gumerov <gumerovm74@gmail.com>
2020-07-01 11:05:44 +0300

    Добавление индикатора загрузки

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import {NavigationComponent} from './components/navigation.component'
 import {CreateComponent} from './components/create.component'
 import {FavoriteComponent} from './components/favorite.component'
 import {PostsComponent} from './components/posts.component'
+import {LoaderComponent} from './components/loader.component'
 
-
+// Валидация формы часть время  3:24
 
 //----------------------------------------------------------------------------
 // ВАЖНАЯ СТРОКА ДЛЯ ИМПОРТА ФАЙЛОВ В БИЛД НЕ УДАЛЯТь
@@ -15,13 +16,24 @@ require.context('./', true, /\.(png|svg|jpg|gif)$/);
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+const loader = new LoaderComponent('loader')
 
-const posts = new PostsComponent('posts')
+const posts = new PostsComponent('posts', {
+  loader
+})
 const create = new CreateComponent('create')
 const favorite = new FavoriteComponent('favorite')
 
-navigation.registerTabs([
-  {name: 'create', component: create},
-  {name: 'posts', component: posts},
-  {name: 'favorite', component: favorite}
-]) 
+navigation.registerTabs([{
+    name: 'create',
+    component: create
+  },
+  {
+    name: 'posts',
+    component: posts
+  },
+  {
+    name: 'favorite',
+    component: favorite
+  }
+])
